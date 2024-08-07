4e39472e9e2db7a2be63b3d6cc09ae1be3f170c3
nathandg <nathandgteam@gmail.com>
2024-08-06 22:36:30 -0300

    Implementar algoritmo de ordenação merge sort

--- ordenacao.c
+++ ordenacao.c
@@ -6,7 +6,8 @@
 void bubble_sort(int arr[], int n) {
     for (int i = 0; i < n - 1; i++) {
         for (int j = 0; j < n - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) { int temp = arr[j];
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
             }
@@ -28,7 +29,7 @@ void insertion_sort(int arr[], int n) {
 }
 
 void selection_sort(int arr[], int n) {
-     for (int i = 0; i < n - 1; i++) {
+    for (int i = 0; i < n - 1; i++) {
         int min_index = i;
         for (int j = i + 1; j < n; j++) {
             if (arr[min_index] > arr[j]) {
@@ -78,6 +79,61 @@ void quick_sort(int arr[], int n) {
     }
 }
  
+void merge(int arr[], int l, int m, int r) {
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
+
+    int *L = (int *)malloc(n1 * sizeof(int));
+    int *R = (int *)malloc(n2 * sizeof(int));
+
+    for (i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+
+    i = 0;
+    j = 0;
+    k = l;
+
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        } else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+
+    free(L);
+    free(R);
+}
+
+void mergeSort(int arr[], int l, int r) {
+    if (l < r) {
+        int m = l + (r - l) / 2;
+
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+
+        merge(arr, l, m, r);
+    }
+}
+
 void gerar_array(int arr[], int tamanho, char *caso) {
     if (strcmp(caso, "melhor") == 0) {
         for (int i = 0; i < tamanho; i++) {
@@ -85,7 +141,7 @@ void gerar_array(int arr[], int tamanho, char *caso) {
         }
     } else if (strcmp(caso, "medio") == 0) {
         for (int i = 0; i < tamanho; i++) {
-            arr[i] = rand() % tamanho + 1;
+            arr[i] = rand() % tamanho;
         }
     } else if (strcmp(caso, "pior") == 0) {
         for (int i = 0; i < tamanho; i++) {
@@ -108,9 +164,10 @@ int main() {
         int tamanho = tamanhos[i];
         double tempos[3];
 
+        int *arr = (int *)malloc(tamanho * sizeof(int));
+
         for (int j = 0; j < num_casos; j++) {
             char *caso = casos[j];
-            int *arr = (int *)malloc(tamanho * sizeof(int));
 
             gerar_array(arr, tamanho, caso);
 
@@ -118,15 +175,15 @@ int main() {
             // insertion_sort(arr, tamanho);
             // bubble_sort(arr, tamanho);
             // selection_sort(arr, tamanho);
-            quick_sort(arr, tamanho);
+            // quick_sort(arr, tamanho);
+            mergeSort(arr, 0, tamanho - 1);
             clock_t fim = clock();
 
             tempos[j] = ((double)(fim - inicio)) / CLOCKS_PER_SEC;
-            // printf("Com %d elementos no %s caso, o tempo foi de %.6f seg.\n", tamanho, caso, tempos[j]);
-            free(arr);
         }
 
         printf("| %d | %.6f seg. | %.6f seg. | %.6f seg. |\n", tamanho, tempos[0], tempos[1], tempos[2]);
+        free(arr);
     }
 
     return 0;
